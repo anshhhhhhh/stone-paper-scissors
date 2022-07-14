8f90bc77d5eaa7126ece7ab8ca1e2a34545340ed
Ansh <anshbansal19906@gmail.com>
2022-07-14 15:23:06 +0530

    added ui

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 let i;
 let playerScore = 0;
 let compScore = 0;
+let round = 0;
+let k = 0;
+
+const buttons = document.querySelectorAll('.play');
+const playerscored = document.querySelector('#playerscore');
+const compscored = document.querySelector('#compscore');
+const result = document.querySelector('#result');
+const final = document.querySelector('#finalresult');
+const reset = document.querySelector('#reset');
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', ()=>{
+        if(round<=4){
+        let playerSelection = button.id;
+        let computerSelection = computerPlay();
+        result.textContent = playRound(computerSelection, playerSelection);
+        playerscored.textContent = `playerscore:${playerScore}`;
+        compscored.textContent = `computerscore:${compScore}`;
+        }
+
+
+        else{
+            k = 1;
+            console.warn("reload");
+        }
+
+        if(round==5 && k==0){
+            if(compScore==playerScore){final.textContent = "It's a draw.";}
+            else if(compScore>=playerScore){final.textContent = `computer beat you by ${compScore}-${playerScore}`;}
+            else{final.textContent = `you beat computer by ${playerScore}-${compScore}`};
+        }
+    });
+
+});
+
+reset.addEventListener('click', ()=>{
+    round=0;
+    playerScore=0;
+    compScore=0;
+    k=0;
+    result.textContent="";
+    final.textContent="";
+    playerscored.textContent = `playerscore:${playerScore}`;
+    compscored.textContent = `computerscore:${compScore}`;
+});
 
 function computerPlay(){
     let x = Math.floor((Math.random()*3));
@@ -15,8 +61,10 @@ function computerPlay(){
 }
 
 function playRound(computerSelection, playerSelection){
+    round++;
     if(computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'paper'){
         i = 2;
+        playerScore++;
         return "You win! Paper beats rock.";
     }
     else if(computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'rock'){
@@ -25,6 +73,7 @@ function playRound(computerSelection, playerSelection){
     }
     else if(computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'scissors'){
         i = 0;
+        compScore++;
         return "You lose! Rock beats scissors.";
     }
     else if(computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'paper'){
@@ -33,18 +82,22 @@ function playRound(computerSelection, playerSelection){
     }
     else if(computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'rock'){
         i = 0;
+        compScore++;
         return "You lose! Paper beats rock.";
     }
     else if(computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'scissors'){
         i = 2;
+        playerScore++;
         return "You win! Scissors beats paper.";
     }
     else if(computerSelection.toLowerCase() === 'scissors' && playerSelection.toLowerCase() === 'paper'){
         i = 0;
+        compScore++;
         return "You lose! Scissors beats paper.";
     }
     else if(computerSelection.toLowerCase() === 'scissors' && playerSelection.toLowerCase() === 'rock'){
         i = 2;
+        playerScore++;
         return "You win! rock beats scissors.";
     }
     else {
@@ -53,28 +106,3 @@ function playRound(computerSelection, playerSelection){
     }
 }
 
-function game(){
-    for(let a = 0; a<5; a++){
-        let computerSelection = computerPlay();
-        let playerSelection = prompt("Choose from stone, paper or scissors.");
-        console.log(playRound(computerSelection, playerSelection));
-        if(i == 2){
-            playerScore = playerScore + 1;
-        }
-        else if(i == 0){
-            compScore = compScore + 1;
-        }
-    }
-
-    if(playerScore > compScore){
-        console.log("You beat computer by " + playerScore + " - " + compScore );
-    }
-    else if (compScore > playerScore){
-        console.log("Computer beat you by " + compScore + " - " + playerScore );
-    }
-    else{
-        console.log("There was a tie between you and computer")
-    }
-}
-
-game();
